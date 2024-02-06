521623e170d591cffcfa76b4effab5ab312bb1ce
danigm11 <danigm821@gmail.com>
2024-02-06 16:13:03 +0100

    Creación bbdd

---
+++ bd.py
@@ -0,0 +1,15 @@
+import sqlite3
+import json
+
+with open('config.json', 'r') as myfile:
+
+   data=myfile.read()
+obj = json.loads(data)
+
+print (obj["dbname"])
+
+con = sqlite3.connect(obj["dbname"])
+
+cur = con.cursor()
+
+res = cur.execute("SELECT name FROM sqlite_master")
