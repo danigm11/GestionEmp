7ef047c136ec53084e0d0180961ed7037c7b62bb
Dani Martín Sáiz <danimartinsaiz@gmail.com>
2024-02-20 00:12:06 +0100

    End points finalizados, archivo sql y readme completo

--- TwoGs.py
+++ TwoGs.py
@@ -1,17 +1,63 @@
-from flask import Flask
+from flask import Flask, request
+import sqlite3
+import json 
+import equipos
 
 app = Flask(__name__)
 
+# End points tabla equipos // Daniel Martín Sáiz
 
-@app.route('/')
-def hello():
-    return "<p>Hello, World!</p>"
+with open('config.json', 'r') as myfile:
+    data=myfile.read()
+obj = json.loads(data)
+
+# Muestra la lista de todos los equipos.
+@app.route('/equipos', methods=['GET'])
+def lista_equipos():
+    return equipos.get_equipos()
+
+#  Muestra los datos del equipo con esa id.
+@app.route('/equipos/<int:id>', methods=['GET'])
+def obtener_equipo(id):
+    return equipos.obtener_equipo(id)
+
+# Muestra los dados del equipo con ese nombre, variable hay que pasarla mediante Params
+@app.route('/equipos/filtrar_por_nombre', methods=['GET'])
+def filtrar_equipos_por_nombre():
+    nombre_equipo = request.args.get('nombre')
+    return equipos.listaEquiposPorNombre(nombre_equipo)
 
 
-@app.route('/torneos')
-def torneo():
-    return "<p>Torneos</p>"
+# Muestra los dados del equipo con más mundiales que los pasados por id, variable hay que pasarla mediante Params
+@app.route('/equipos/filtrar_por_titulos_mundiales', methods=['GET'])
+def filtrar_equipos_por_titulos_mundiales():
+    num_titulos = request.args.get('num_titulos')
+    return equipos.listaEquiposConMasDeNTitulosMundiales(num_titulos)
 
-@app.route('/equipos')
-def equipos():
-    return "<pEquipos</p>"
+# Añadir equipo
+@app.route('/equipos', methods=['POST'])
+def añadir_equipo():
+    return equipos.add_equipo()
+
+# Modificar equipo
+@app.route('/equipos/<int:id>', methods=['PUT'])
+def modificar_equipo(id):
+    return equipos.update_equipo(id)
+
+# Borrar equipo
+@app.route('/equipos/<int:id>', methods=['DELETE'])
+def borrar_equipo(id):
+    return equipos.delete_equipo(id)
+
+# Ver jugadores en un equipo, conexión entre tablas
+@app.route('/equipos/<int:id_equipo>/jugadores', methods=['GET'])
+def obtener_jugadores_equipo(id_equipo):
+    return equipos.get_jugadores(id_equipo)
+
+# Añadir jugadores, necesario para la petición anterior
+@app.route('/jugadoresD', methods=['POST'])
+def nuevo_jugador():
+    return equipos.add_jugador()
+
+# FÍN End points tabla equipos // Daniel Martín Sáiz
+
---
+++ TwoGs.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE IF NOT EXISTS TwoGs;
+
+USE TwoGs;
+
+CREATE TABLE IF NOT EXISTS Torneos (
+    ID INTEGER NOT NULL UNIQUE,
+    Nombre TEXT,
+    Sede TEXT,
+    Fecha TEXT,
+    Premio NUMERIC,
+    Categoria TEXT,
+    PRIMARY KEY(ID)
+);
+
+CREATE TABLE IF NOT EXISTS Equipos (
+    ID INTEGER NOT NULL UNIQUE,
+    Nombre TEXT,
+    Región TEXT,
+    Titulos_Mundiales INTEGER,
+    Titulos_Regionales INTEGER,
+    Categoría INTEGER,
+    PRIMARY KEY(ID)
+);
+
+CREATE TABLE "Jugadores" (
+	"ID"	INTEGER UNIQUE,
+	"Nombre"	TEXT,
+	"Nacionalidad"	BLOB,
+	"Edad"	INTEGER,
+	"Rol"	TEXT,
+	"IDEquipo"	INTEGER,
+	PRIMARY KEY("ID" AUTOINCREMENT),
+	FOREIGN KEY("IDEquipo") REFERENCES "Equipos"("ID")
+)
